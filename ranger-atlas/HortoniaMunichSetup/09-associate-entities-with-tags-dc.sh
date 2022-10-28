927185e0e0e5a0e69b18751838366155771876cd
Laurent Edel <laurent.edel@gmail.com>
2022-10-28 02:00:50 +0200

    Update 09-associate-entities-with-tags-dc.sh
    silent curl

--- ranger-atlas/HortoniaMunichSetup/09-associate-entities-with-tags-dc.sh
+++ ranger-atlas/HortoniaMunichSetup/09-associate-entities-with-tags-dc.sh
@@ -1,7 +1,7 @@
 atlas_host=${atlas_host:-$(hostname -f)}
 atlas_pass=${atlas_pass:-admin}
 
-atlas_curl="curl -u admin:${atlas_pass}"
+atlas_curl="curl -s -u admin:${atlas_pass}"
 atlas_url="http://${atlas_host}:31000/api/atlas"
 
 cluster_name=cm
@@ -16,7 +16,6 @@ ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"REFERENCE_DATA","values":{}}'
 
-
 #fetch guid for table consent_master.eu_countries@${cluster_name}
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_table?attr:qualifiedName=consent_master.eu_countries@${cluster_name} | jq '.entity.guid'  | tr -d '"')
 
@@ -25,8 +24,6 @@ ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"REFERENCE_DATA","values":{}}'
 
-
-
 #fetch guid for table consent_master.consent_data
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_table?attr:qualifiedName=consent_master.consent_data@${cluster_name} | jq '.entity.guid'  | tr -d '"')
 
@@ -34,7 +31,6 @@ guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_table?att
 ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"REFERENCE_DATA","values":{}}'
-
 
 #fetch guid for table consent_master.consent_data_trans
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_table?attr:qualifiedName=consent_master.consent_data_trans@${cluster_name} | jq '.entity.guid'  | tr -d '"')
@@ -45,10 +41,7 @@ ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"REFERENCE_DATA","values":{}}'
 
 
-
-	
 ## tag hive tables with attribute
-
 
 #fetch guid for table cost_savings.claim_savings@${cluster_name}
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_table?attr:qualifiedName=cost_savings.claim_savings@${cluster_name} | jq '.entity.guid'  | tr -d '"')
@@ -57,8 +50,6 @@ guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_table?att
 ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"DATA_QUALITY", "values":{"score": "0.51"}}'
-
-
 
 ## Tagging Hive columns
 
@@ -70,7 +61,6 @@ ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"VENDOR_PII", "values":{"type": "vendor"}}'
 
-
 #fetch guid for  finance.tax_2015.ssn
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?attr:qualifiedName=finance.tax_2015.ssn@${cluster_name} | jq '.entity.guid'  | tr -d '"')
 
@@ -78,7 +68,6 @@ guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?at
 ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"FINANCE_PII", "values":{"type": "finance"}}'
-
 
 #fetch guid for finance.tax_2015.fed_tax
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?attr:qualifiedName=finance.tax_2015.fed_tax@${cluster_name} | jq '.entity.guid'  | tr -d '"')
@@ -88,7 +77,6 @@ ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"EXPIRES_ON", "values":{"expiry_date": "2016-12-31T00:00:00.000Z"}}'
 
-
 #fetch guid for worldwidebank.us_customers.ccnumber
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?attr:qualifiedName=worldwidebank.us_customers.ccnumber@${cluster_name} | jq '.entity.guid'  | tr -d '"')
 
@@ -97,7 +85,6 @@ ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"PII", "values":{"type": "ccn"}}'
 
-
 #fetch guid for worldwidebank.us_customers.mrn
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?attr:qualifiedName=worldwidebank.us_customers.mrn@${cluster_name} | jq '.entity.guid'  | tr -d '"')
 
@@ -105,7 +92,6 @@ guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?at
 ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"PII", "values":{"type": "MRN"}}'
-
 
 #fetch guid for worldwidebank.us_customers.nationalid
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?attr:qualifiedName=worldwidebank.us_customers.nationalid@${cluster_name} | jq '.entity.guid'  | tr -d '"')
@@ -134,7 +120,6 @@ ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"PII", "values":{"type": "Email"}}'
 
-
 #fetch guid for table hr.employees_encrypted.phone@${cluster_name}
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?attr:qualifiedName=hr.employees_encrypted.phone@${cluster_name} | jq '.entity.guid'  | tr -d '"')
 
@@ -143,8 +128,6 @@ ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"ENCRYPTED", "values":{"type": "phone"}}'
 
-
- 
 #fetch guid for table hr.employees_encrypted.email@${cluster_name}
 guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?attr:qualifiedName=hr.employees_encrypted.email@${cluster_name} | jq '.entity.guid'  | tr -d '"')
 
@@ -152,7 +135,6 @@ guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hive_column?at
 ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"ENCRYPTED", "values":{"type": "email"}}'
-
 
 #create entities for kafka topic FOREX
 ${atlas_curl}  ${atlas_url}/v2/entity -X POST -H 'Content-Type: application/json' -d @- <<EOF
@@ -230,9 +212,8 @@ ${atlas_curl}  ${atlas_url}/v2/entity -X POST -H 'Content-Type: application/json
 }
 EOF
 
-guid=$(${atlas_curl}  ${atlas_url}/v2/entity/uniqueAttribute/type/hdfs_path?attr:qualifiedName=${hdfs_prefix}${hdfs_path} | jq '.entity.guid'  | tr -d '"')
+guid=$(${atlas_curl} ${atlas_url}/v2/entity/uniqueAttribute/type/hdfs_path?attr:qualifiedName=${hdfs_prefix}${hdfs_path} | jq '.entity.guid'  | tr -d '"')
 
 ${atlas_curl} ${atlas_url}/entities/${guid}/traits \
 -X POST -H 'Content-Type: application/json' \
 --data-binary '{"jsonClass":"org.apache.atlas.typesystem.json.InstanceSerialization$_Struct","typeName":"SENSITIVE","values":{}}'
-
